db19f6a81ed23fad9217c3eb8ef22666ca81aaa7
BRES Raphael <rbres@orthopedix.pedago.ipb.fr>
2019-12-04 14:37:02 +0100

    src first

---
+++ src/threshold.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char* argv[] )
+{
+    // boucle sur les images
+        // inrange sur l'image n
+        // extraire forme des points les plus externes parmi les thresholdé
+  
+    return EXIT_SUCCESS;  
+}
