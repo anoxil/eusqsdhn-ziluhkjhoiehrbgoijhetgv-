e504ecfb3e2210c513f08c00cd8aed649570c0b3
Raphael BRES <bres.raphael@gmail.com>
2019-12-04 15:18:57 +0100

    ims read test

--- src/threshold.cpp
+++ src/threshold.cpp
@@ -3,11 +3,21 @@
 using namespace cv;
 using namespace std;
 
-int main( int argc, char* argv[] )
+void process(const char* ims_name)
 {
-    // boucle sur les images
-        // inrange sur l'image n
-        // extraire forme des points les plus externes parmi les thresholdé
-  
-    return EXIT_SUCCESS;  
+    Mat ims = imread("../data/log1/001-rgb.png", CV_LOAD_IMAGE_COLOR);
+    Mat ims_hsv; cvtColor(ims, ims_hsv, CV_RGB2HSV);
+}
+
+int main(int argc, char* argv[])
+{
+    /* 
+    boucle sur les images
+        inrange sur l'image n
+        extraire forme des points les plus externes parmi les thresholdés
+    */
+
+   process(argv[1]);
+
+   return EXIT_SUCCESS;  
 }
